0f115d14db70642e05afc1c0d78a0d2a5964873f
Andre Dean <Andre@Andres-Retina-MacBook-Pro.local>
2017-09-24 16:37:50 -0700

    Add functionality to previous track button

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -10,8 +10,19 @@ $( document ).ready(function() {
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const nextSongIndex = currentSongIndex + 1;
     if (nextSongIndex >= album.songs.length) {return;}
-    
+
     const nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong);
+  });
+
+  $('button#previous').click(function() {
+    if(player.playState !== 'playing') { return; }
+
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const lastSongIndex = currentSongIndex - 1;
+    if (lastSongIndex < 0) { return; }
+
+    const lastSong = album.songs[lastSongIndex];
+    player.playPause(lastSong);
   });
 });
