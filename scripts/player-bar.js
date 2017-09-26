8d936dddf8799ddb4bfeffb23beb80f7f7c66822
Andre Dean <Andre@Andres-Retina-MacBook-Pro.local>
2017-09-25 21:21:10 -0700

    Add time slider, and current time functionality

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -25,4 +25,16 @@ $( document ).ready(function() {
     const lastSong = album.songs[lastSongIndex];
     player.playPause(lastSong);
   });
+
+  $('#time-control input').on('input', function(event) {
+    player.skipTo(event.target.value);
+  });
+
+  setInterval( () => {
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control .current-time').text(currentTime);
+    $('#time-control input').val(percent);
+  }, 1000);
 });
